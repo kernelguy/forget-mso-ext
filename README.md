76aba9780a06483a0c9f884d72e9a4a91a95f0e5
kernelguy <steffen@rspsystems.com>
2025-11-19 10:20:02 +0100

    Initial extension implemented

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # forget-mso-ext
-Firefox extension to remove outlook.com cookies
+Firefox extension to remove outlook.com cookies and active login
+
+This extension fixes the annoying recursive "Please wait while we log you out..." problem, when using outlook.office.com in Firefox with persistent tabs.
+
---
+++ forget-mso.js
@@ -0,0 +1,33 @@
+/*
+Generic error logger.
+*/
+function onError(e) {
+  console.error(e);
+}
+
+function onRemoved() {
+  console.log("Removed cookies for office.com");
+}
+
+/*
+Forget cookies for office.com, to avoid circular "Please wait while we log you out shit..."
+*/
+function forget() {
+  let options = {
+    hostnames: ["office.com", "outlook.office.com"]
+  }
+  
+  let types = {
+    cookies: true,
+    localStorage: true,
+    passwords: true
+  }
+  
+  browser.browsingData.remove(options, types).then(onRemoved, onError);
+}
+
+/*
+On click, forget cookies for office.com.
+*/
+browser.browserAction.onClicked.addListener(forget);
+
